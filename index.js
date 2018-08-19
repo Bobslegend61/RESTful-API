6bfab2f92d0d2b555df3e25c00c3d90be8ba2400
Alabi Emmanuel <bobslegend795@gmail.com>
2018-08-19 11:55:20 +0100

    Starting up a server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+/**
+ * The Primary File For The API
+ */
+
+//  Dependencies
+let http = require('http');
+
+// The server should respond to all requests with a string
+let server = http.createServer((req, res) => {
+    res.end('Hello world\n');
+});
+
+// Start the server, and have it listen on port 3000
+server.listen(3000, () => console.log('The server is listening on port 3000'));

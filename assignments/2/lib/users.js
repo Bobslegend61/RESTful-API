33ab5ed2cc044e09d6d243670ca134acf346f6bb
Alabi Emmanuel <bobslegend795@gmail.com>
2018-08-31 07:28:58 +0100

    Created functions for creating and updatinga user

--- assignments/2/lib/users.js
+++ assignments/2/lib/users.js
@@ -1,3 +1,37 @@
 /**
  * Lib for users
  */
+const path = require('path');
+
+const { create, update } = require('./helpers');
+
+const users = (() => {
+    const baseDir = path.join(__dirname, '/../.data/users/');
+
+    /**
+     * Creates a user in the file syatem
+     *
+     * @param { string } fileName - Name of the file to store the user.
+     * @param { string } data - Details of the user.
+     * @param { Object } callback - function to return error if any.
+     */
+    const createUser = (fileName, data, callback) => {
+        create(baseDir+fileName, '.json', data, callback);
+    };
+
+    /**
+     * Updates a user in the file system.
+     *
+     * @param { string } fileName - Name of the user file to update.
+     * @param { string } data - Details of the user.
+     * @param { Object } callback - function to return error if any.
+     */
+    const updateUser = (fileName, data, callback) => {
+        update(baseDir+fileName, '.json', data, callback);
+    };
+
+    return {
+        createUser,
+        updateUser
+    }
+})();
